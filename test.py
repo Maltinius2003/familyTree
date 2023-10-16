f455f435e7dad755bcbb10587e0bcee1d7e9cdf1
Maltinius <bananenbub1@gmail.com>
2023-10-16 11:22:37 +0200

    .

--- test.py
+++ test.py
@@ -3,15 +3,14 @@ import helpMethods as hm
 with open('religions.csv') as f:
     reader = csv.reader(f)
     religions = list(reader)
-#print(religions)
 
 longestLength = len(hm.getLongestListinList(religions))
 orderdReligions = []
 for i in range(longestLength-1): orderdReligions.append([])
 
 for list in religions:
-    for i in range(2, longestLength+1): #Gibt nur zweiteilige Spalten
+    for i in range(2, longestLength+1): #Gibt nur mindestens zweiteilige Spalten
         if len(list) == i:
             orderdReligions[i-2].append(list)
 
-print(orderdReligions)
+print(orderdReligions[0])
